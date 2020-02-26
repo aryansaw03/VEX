205f6e9e7796a72b21a0623b328b66962292a424
aryan <aryansaw03@gmail.com>
2020-02-25 20:54:26 -0500

    jaujt

--- src/Autonomous.cpp
+++ src/Autonomous.cpp
@@ -31,12 +31,22 @@ void runCollectCubes(){
 }
 
 void testAuton(){
-	//turnToHeadingPD(degToRad(180),190, 35, getMaxVelocity(chassisLeftBack));
+	// turnToHeadingPD(degToRad(270),60, 5, 60);
+	// delay(50);
+	// turnToHeadingPD(degToRad(0),60, 5, 60);
 
-	moveToPositionPD(1, 1,
-		190, 35, 100,
-		190, 35, 100,
-		0.05, 0.01, 0.2);
+	//  moveToPositionPD(24, 24, // targetX, targetY
+	// 	190, 35, 100, // pGainTurn, dGainTurn, turnMaxVelocity
+	// 	5, 0, 30, // pGainMove, dGainMove, moveMaxVelocity,
+	// 	0.1, 0.05, 0.4); // pGainCorrection, dGainCorrection, maxCorrection
+
+	swingMoveToPositionPD(0, 24, // targetX, targetY
+		10, 0, 50, // pGainMove, dGainMove, moveMaxVelocity,
+		0.05, 0, 0.8); // pGainCorrection, dGainCorrection, maxCorrection
+
+	// swingTurnToHeadingPD(degToRad(180), // targetHeading
+	// 	130, 0, 30, // pGainMove, dGainMove, moveMaxVelocity,
+	// 	2, 0, 1.0); // pGainCorrection, dGainCorrection, maxCorrection
 }
 
 /**
